a630bb3330d1a3b207cb7077567e134495e7b717
Ben <bendan7@gmail.com>
2019-05-26 13:05:59 +0300

    add respond method

--- Algo2.py
+++ Algo2.py
@@ -1,6 +1,7 @@
 import csv
 import os
 import threading
+import traceback
 
 
 class Algo2():
@@ -36,7 +37,7 @@ class Algo2():
         finally:
             self.recids_file.close()
 
-        self.RECS_THRESHOLD=5
+        self.RECS_THRESHOLD=2
         self.ATTS_NUM=len(self.attsNameArr)
         self.RECS_NUM=len(self.recidsArr)
 
@@ -51,6 +52,9 @@ class Algo2():
         # the number of relevant attribute we currently have
         self.relAttsNum=self.ATTS_NUM
         self.relRecsNum=self.RECS_NUM
+
+        self.ansable = False
+        self.currentAtt = None
 
         return
 
@@ -98,14 +102,21 @@ class Algo2():
                 else:
                     self.giniRates[i] = -1
                 i += 1
-        except:
+        except Exception:
             print("cant find data.csv file")
+            traceback.print_exc()
         finally:
             self.data_file.close()
 
-        max_i = self.giniRates.index(max(self.giniRates))
-        return self.attsNameArr[max_i]
+        self.maxAtt_i = self.giniRates.index(max(self.giniRates))
 
+        # update the current att name for sync purposes
+        self.currentAtt = self.attsNameArr[self.maxAtt_i]
+
+        # enable the option to receive answer
+        self.ansable = True
+
+        return self.attsNameArr[self.maxAtt_i]
 
     def getAttImg(self, name):
         pass
@@ -114,15 +125,68 @@ class Algo2():
         pass
 
     # ans = {name(the name of the att that was answer), ans(y/n}
-    def respond(self,ans):
+    def respond(self, res):
+
+        if not self.ansable:
+            print("The algo get respond before calling to getatt")
+            return "The algo get respond before calling to getatt"
+        if res['name'] != self.currentAtt:
+            print("The respond is out of sync")
+            return "The respond is out of sync"
+
         self.lock.acquire()
 
-        self.lock.release()
+        # update the number of the
+        self.relAttsNum -= 1
+
+        # turn off the relevant att index
+        self.RA[self.maxAtt_i] = 0
+
+        try:
+            self.data_file = open(os.path.join(self.__location__, self.dataDir+'data.csv'),encoding="utf-8-sig")
+            if res['ans'] == "0":
+                reverseRow = list(map((lambda x: 1 if x == 0 else 0),
+                                      [int(x) for x in
+                                       self.readSpecificLine(self.maxAtt_i, self.data_file).split(',')]))
+                self.RR = self.AND(self.RR, reverseRow)
+                print("ans is 0 ")
+
+            if res['ans'] == "1":
+                attRow = list(map(int, self.readSpecificLine(self.maxAtt_i, self.data_file).split(',')))
+                self.RR = self.AND(self.RR, attRow)
+
+            self.relRecsNum=sum(self.RR)
+
+        except Exception:
+            print("Error: Respond()!!@!$!@$@#")
+            traceback.print_exc()
+        finally:
+            self.data_file.close()
+            self.lock.release()
+
         return self.areWeDone()
 
     def areWeDone(self):
-        pass
+        if self.relRecsNum<=self.RECS_THRESHOLD:
+            return True
+        return False
 
+    def readSpecificLine(self, lineNumber, file):
+        file.seek(0)
+        for i, line in enumerate(file):
+            if i == lineNumber:
+                return line
+        return "ERROR: LINE #" + str(lineNumber) + " DOESN'T FOUND"
+
+    # AND operator that work with 2 arrays
+    def AND(self,A, B):
+        leng = len(A)
+        for i in range(0, leng):
+            if A[i] == 1 & B[i] == 1:
+                A[i] = 1
+            else:
+                A[i] = 0
+        return A
 
     def recPreview(self):
         return [{},{}]
--- testAlgo2.py
+++ testAlgo2.py
@@ -1,6 +1,21 @@
 from Algo2 import Algo2
 
-algo2 = Algo2()
+def testAlgoWithQuestions():
+    algo = Algo2()
+    while True:
+        # print(algo.getNumOfRelevantDishes())
+        name = algo.getNextAtt()
+        ans = input('got ' + name + '?\n')
+        res = {'name':name, 'ans':ans}
+        x = algo.respond(res)
+        if x:
+            print("finish")
+            break
 
-x = algo2.getNextAtt()
-print(x)
+
+# algo2 = Algo2()
+#
+# x = algo2.getNextAtt()
+# print(x)
+# algo2.respond()
+testAlgoWithQuestions()
